e804829c124c75dfe546336d4603670d6492509c
Javier Gomez <javiergomez@Javiers-iMac.local>
2020-03-21 21:32:46 -0500

    BETA RC001 Build 011

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
-import 'mapbox-gl/dist/mapbox-gl.css';
+
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
@@ -27,8 +27,8 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
+        <Route path="/" component={Home} exact={true} />
+ 
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
