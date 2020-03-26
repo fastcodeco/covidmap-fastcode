fb4eb3be5beafb428252ce02fab2ec0918b7c6d2
Javier Gomez <javiergomez@Javiers-iMac.local>
2020-03-25 20:22:29 -0500

    BETA RC001 Build RC042

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -87,7 +87,7 @@ class Map extends React.Component<any, any>{
             this.setState({
                 viewport: {
                     width: '100vw',
-                    height: '90vh',
+                    height: '75vh',
                     longitude: geolocation.coords.longitude,
                     latitude: geolocation.coords.latitude,
                     zoom: 5,
@@ -117,7 +117,7 @@ class Map extends React.Component<any, any>{
             this.setState({
                 viewport: {
                     width: '100vw',
-                    height: '85vh',
+                    height: '80vh',
                     longitude: geolocation.coords.longitude,
                     latitude: geolocation.coords.latitude,
                     zoom: 13,
@@ -152,7 +152,7 @@ class Map extends React.Component<any, any>{
             onViewportChange={this._onViewportChange}
             mapboxApiAccessToken='pk.eyJ1IjoiZmFzdGNvZGUiLCJhIjoiY2s4MGczdmNxMGFybzNkc2Z4M24wYWhqZyJ9.xo6Amo_Nh2ZbxoNDq5t7BQ'
             width='100vw'
-            height='85vh'
+            height='80vh'
             center = {[this.state.viewport.latitude, this.state.longitude]}
             mapStyle="mapbox://styles/mapbox/dark-v9"
 
